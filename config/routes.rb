c8b7d4bddc42199598b35d368e2e0536a6af03aa
Arne Pedersen <arne.lamo@gmail.com>
2019-07-16 09:51:14 +0200

    solved merging conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-<<<<<<< HEAD
-  resources :babysitters, only: [:index, :show]
-  resource :dashboard, only: [:show]
-=======
   devise_for :users
 
+  resource :dashboard, only: [:show]
   resources :babysitters, only: [:index, :show] do
     resources :bookings, only: [:create]
   end
->>>>>>> d4d7f2cc5e08feb4fdb4c21eae4a1668a2ae3c77
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
