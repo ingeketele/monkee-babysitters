a93a8ee62b87b6718736dd020619117014e40cfa
Arne Pedersen <arne.lamo@gmail.com>
2019-07-16 12:24:34 +0200

    added review model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :babysitters, only: [:index, :show] do
     resources :bookings, only: [:create]
   end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_062418) do
+ActiveRecord::Schema.define(version: 2019_07_16_082818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2019_07_16_062418) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "booking_id"
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +66,5 @@ ActiveRecord::Schema.define(version: 2019_07_16_062418) do
   add_foreign_key "babysitters", "users"
   add_foreign_key "bookings", "babysitters"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
