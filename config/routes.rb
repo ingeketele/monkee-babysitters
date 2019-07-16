d18926549fff88565bdc076c1032f4f63f62531a
Arne Pedersen <arne.lamo@gmail.com>
2019-07-16 09:33:23 +0200

    before pull from babysitter-detail

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :babysitters, only: [:index, :show]
+  resource :dashboard, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
