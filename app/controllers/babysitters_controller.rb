72ed1acd50212c430b8f2dd8a3f43ae6cc9111c9
Manuel Velasco <velascome@gmail.com>
2019-07-17 10:05:53 +0200

    Filter by price, cards link to show details, FE cards

--- app/controllers/babysitters_controller.rb
+++ app/controllers/babysitters_controller.rb
@@ -1,5 +1,13 @@
 class BabysittersController < ApplicationController
   def index
+    price_filter = params.dig(:sort, :price_per_hour)
     @babysitters = Babysitter.all
+    if price_filter == "Lowest"
+      @babysitters = @babysitters.order(price_per_hour: :asc)
+    elsif price_filter == "Highest"
+      @babysitters = @babysitters.order(price_per_hour: :desc)
+    elsif price_filter == "All"
+      @babysitters = Babysitter.all
+    end
   end
 end
--- app/models/babysitter.rb
+++ app/models/babysitter.rb
@@ -2,4 +2,5 @@ class Babysitter < ApplicationRecord
   belongs_to :user
 
   validates :biography, :birthdate, :price_per_hour, presence: true
+
 end
