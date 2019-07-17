96774d6bb9be4aa0939f71ea95d157e07a63e940
Arne Pedersen <arne.lamo@gmail.com>
2019-07-17 12:27:08 +0200

    mapbox map with markers and zoom working

--- app/controllers/babysitters_controller.rb
+++ app/controllers/babysitters_controller.rb
@@ -3,13 +3,20 @@ class BabysittersController < ApplicationController
 
   def index
     price_filter = params.dig(:sort, :price_per_hour)
-    @babysitters = Babysitter.all
+    @babysitters = Babysitter.geocoded
     if price_filter == "Lowest"
       @babysitters = @babysitters.order(price_per_hour: :asc)
     elsif price_filter == "Highest"
       @babysitters = @babysitters.order(price_per_hour: :desc)
     elsif price_filter == "All"
-      @babysitters = Babysitter.all
+      @babysitters = Babysitter.geocoded
+    end
+
+    @markers = @babysitters.map do |babysitter|
+      {
+        lat: babysitter.user.latitude,
+        lng: babysitter.user.longitude
+      }
     end
   end
 
